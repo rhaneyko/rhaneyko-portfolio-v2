a67529db760b2153a4d6e5f4981dcf96e5eb670a
rhaneyko <rhaneykohonrio@gmail.com>
2022-09-06 12:40:28 -0400

    React App

--- src/screens/SkillsPage/index.tsx
+++ src/screens/SkillsPage/index.tsx
@@ -1,6 +1,7 @@
 import { Container, Title, About, TextAbout } from "./styles";
 
-import { SiCss3, SiFirebase, SiJavascript, SiReact, SiTypescript, SiHtml5,  } from 'react-icons/si'
+import { SiCss3, SiFirebase, SiJavascript, SiReact, SiTypescript, SiHtml5,  } from 'react-icons/si';
+
 
 const SkillsPage: React.FC = () => {
   return (
@@ -9,9 +10,11 @@ const SkillsPage: React.FC = () => {
         <Title>Conhecimentos</Title>
          <SiHtml5
            size={50}
+           color='#E34C26'
          />
          <SiCss3
            size={50}
+           color='#264DE4'
          />
          <SiJavascript
            size={50}
@@ -19,14 +22,16 @@ const SkillsPage: React.FC = () => {
          />
          <SiReact
            size={50}
+           color='#55DDED'
          />
          <SiTypescript
            size={50}
+           color='#235A97'
          />
          <SiFirebase
            size={50}
+           color='#FFC526'
          />
-         
       </About>
     </Container>
   );
