8b37e3a523f5f480003f356828308d852f3bd641
rhaneyko <rhaneykohonrio@gmail.com>
2022-09-08 12:06:18 -0400

    React App

--- src/screens/SkillsPage/index.tsx
+++ src/screens/SkillsPage/index.tsx
@@ -1,20 +1,32 @@
-import { Container, Title, Icon, Skills, TextAbout } from "./styles";
+import { 
+  Container, 
+  Title, 
+  Skills, 
+  Icons,
+  Icon, 
+  TextAbout, 
+  DescriptionAboutSkill,
+} from "./styles";
 
 import { SiCss3, SiFirebase, SiJavascript, SiReact, SiTypescript, SiHtml5,  } from 'react-icons/si';
 
 
 const SkillsPage: React.FC = () => {
+
   return (
     <Container id="about">
-      <Skills>
         <Title>Conhecimentos</Title>
+      <Skills>
+        <Icons>
          <Icon>
            <SiHtml5
              size={50}
              color='#E34C26'
              className='icons'
            />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> 
          </Icon>
          <Icon>
            <SiCss3
@@ -22,7 +34,9 @@ const SkillsPage: React.FC = () => {
              color='#264DE4'
              className='icons'
            />
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           {/* <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> */}
          </Icon>
          <Icon>
            <SiJavascript
@@ -30,7 +44,10 @@ const SkillsPage: React.FC = () => {
              color='#F0DB4F'
              className='icons'
            />
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusation adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           
+           {/* <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> */}
          </Icon>
          <Icon>
            <SiReact
@@ -38,7 +55,9 @@ const SkillsPage: React.FC = () => {
              color='#55DDED'
              className='icons'
             />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+            {/* <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> */}
           </Icon>
           <Icon>
          <SiTypescript
@@ -46,7 +65,9 @@ const SkillsPage: React.FC = () => {
            color='#235A97'
            className='icons'
          />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+          {/* <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> */}
          </Icon>
          <Icon>
            <SiFirebase
@@ -54,8 +75,12 @@ const SkillsPage: React.FC = () => {
              color='#FFC526'
              className='icons'
            />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+          {/* <TextAbout>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elite. Quam accusantium adipisci ipsam placeat? Corporis aspernatur iusto ea molestias reiciendis consequatur ratione quibusdam dolores cupiditate a. Molestiae maiores aliquid fugiat accusantium?</p>
+           </TextAbout> */}
          </Icon>
+        </Icons>
+         <DescriptionAboutSkill></DescriptionAboutSkill>
       </Skills>
     </Container>
   );
--- src/screens/SkillsPage/styles.ts
+++ src/screens/SkillsPage/styles.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
-    height: 80vh;
+    flex-direction: column;
+    height: 100vh;
 
     justify-content: center;
     align-items: center;
+
+    background-color: blue;
 
     @media (max-width: 768px) {
         width: 125%;
@@ -16,15 +19,16 @@ export const Container = styled.div`
 export const Skills = styled.div`
     display: flex;
     flex-direction: column;
-    width: 100%;
-    max-width: 40rem;
-    margin-bottom: 3rem;
+    align-items: center;
+    
+    width: 700px;
+    height: 500px;
 
+    margin-bottom: 3rem;
     background-color: red;
 
     .icons{
         margin: 20px;
-
 
         :hover{
             p{
@@ -41,47 +45,71 @@ export const Skills = styled.div`
     }
 `;
 
+export const Icons = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
 export const Icon = styled.div`
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+
+    background-color: white;
 
 
     height: 100px;
 
     margin: 10px;
 
-    
+
+    p{
+        display: none;
+    }
 
     :hover{
         width: 80%;
 
         p{
             display: inline;
-            width: 400px;
 
             text-align: justify;
         }
     }
-
-
 `;
 
 export const Title = styled.h1`
     display: flex;
-
-    margin-bottom: 2%;
-
     
     font-size: 2rem;
     line-height: 3rem;
     color: ${props => props.theme.colors.title};
+    
+    margin-bottom: 2%;
 
     @media (max-width: 768px) {
         font-size: 1.5rem;
     }
 `;
 
-export const TextAbout = styled.p`
+export const TextAbout = styled.div`
+    display: none;
+    width: 500px;
+
+    text-align: justify;
+    background-color: blue;
+    
+    @media (max-width: 768px) {
+        font-size: 0.8rem;
+    }
+`;
+
+export const DescriptionAboutSkill = styled.div`
+    
+    width: 300px;
+    height: 150px;
+
+    text-align: justify;
+    background-color: purple;
     
     @media (max-width: 768px) {
         font-size: 0.8rem;
